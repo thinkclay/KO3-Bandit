ad8c8beafe52c64409b992464e55eeed5123eb88
Clay McIlrath <clay.mcilrath@gmail.com>
2013-06-05 08:10:45 -0600

    annex config

---
+++ config/bandit_annex.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return [
+    // Module Information
+    'module' => [
+        'name'      => 'Bandit',
+        'overview'  => 'A module for web scrapes',
+        'version'   => '1.0.0',
+        'url'       =>  [
+            'author'    => 'http://thinkclay.com',
+        ],
+
+        // create a point release
+        // levels: update, feature, security
+        'changelog' => [
+            '1.0.0' => ['update' => 'Initial Release of the Module'],
+        ],
+    ]
+];
